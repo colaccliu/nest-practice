58696f47b2b6bef88886551a33057427140616d4
colaliu <colaliu@tencent.com>
2021-04-12 20:51:43 +0800

    feat: add roles guard

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,34 +1,42 @@
-import { Body, ClassSerializerInterceptor, Controller, Get, HttpException, Post, UseFilters, UseInterceptors } from '@nestjs/common'
+import { Body, ClassSerializerInterceptor, Controller, Get, Post, SetMetadata, UseFilters, UseGuards, UseInterceptors } from '@nestjs/common'
 import { CreateCatDto } from './dto/create-cat.dto'
 import { Cat } from './interfaces/cat.interface'
 import { CatsService }  from './cats.service'
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter'
 import { catEntity } from 'src/common/entities/cat.entity'
+import { RolesGuard } from 'src/common/guards/roles.guard'
+import { Roles } from 'src/common/decorators/role.decorator'
 
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
-    constructor(private catsService: CatsService) {}
+  constructor(private catsService: CatsService) {}
 
-    @Get()
-    async findAll():Promise<any> {
-        return this.catsService.findAll()
-    }
+  @Get()
+  //   @SetMetadata('roles', ['admin'])
+  @Roles('admin')
+  async findAll(): Promise<any> {
+    return this.catsService.findAll();
+  }
 
-    @UseInterceptors(ClassSerializerInterceptor)
-    @Get('findOne')
-    async findOne():Promise<any> {
-        return new catEntity({
-            name: 'cola',
-            age: '3',
-            breed: 'miaomiao',
-            color: 'yellow'
-        })
-    }
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('findOne')
+  // @SetMetadata('roles', ['guest'])，用装饰器封装一层
+  @Roles('guest')
+  async findOne(): Promise<any> {
+    return new catEntity({
+      name: 'cola',
+      age: '3',
+      breed: 'miaomiao',
+      color: 'blue',
+    });
+  }
 
-    @Post('add')
-    @UseFilters(HttpExceptionFilter) // 使用类，可以在整个模块中使用同一实例，减少内存使用
-    async create(@Body() createCatDto: CreateCatDto) {
-        this.catsService.create(createCatDto)
-    }
+  @Post('add')
+  @Roles('test')
+  @UseFilters(HttpExceptionFilter) // 使用类，可以在整个模块中使用同一实例，减少内存使用
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
 }
